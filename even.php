a686251161000c7d0d268cf56bc0cc447ade7c2c
nikhilsingh16nikhil-star <nikhil.singh16.nikhil@gmail.com>
2026-06-25 19:36:31 +0530

    Add files via upload

---
+++ even.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=\, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $num = 10;
+    if ($num%2==0){
+        echo("even");
+    }
+    else{
+        echo ("odd");
+    }
+    ?>
+
+</body>
+</html>
